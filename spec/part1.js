49aa8d9251a7d1124f8682faa1370e458161286a
joshawesome12 <joshawesome12@yahoo.com>
2017-10-03 19:39:19 -0500

    Finished underbar part 1

--- spec/part1.js
+++ spec/part1.js
@@ -503,7 +503,7 @@
 
       it('Fill me in with a description of the behavior this test is checking for', function() {
         var result = _.reduce([1, 2, 3], function(memo, item) {
-          return memo *= item;
+          return memo * item;
         }, 0);
 
         expect(result).to.equal(0);
